ca5c75a60a0a4d553f59b4723ba4496bf0f0e575
Sebastian Humenda <shumenda@gmx.de>
2018-04-03 14:53:13 +0200

    Add L4Bender as linker variant

--- src/librustc_codegen_llvm/back/link.rs
+++ src/librustc_codegen_llvm/back/link.rs
@@ -94,6 +94,7 @@ pub fn get_linker(sess: &Session) -> (PathBuf, Command) {
             LinkerFlavor::Gcc => "cc".as_ref(),
             LinkerFlavor::Ld => "ld".as_ref(),
             LinkerFlavor::Lld(_) => "lld".as_ref(),
+            LinkerFlavor::L4Bender => "l4-bender".as_ref(),
         });
 
     let mut cmd = cmd(linker_path);
