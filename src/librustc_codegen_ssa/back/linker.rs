b4837287d2973b4dad7329236cb114ad41a3ad13
Sebastian Humenda <shumenda@gmx.de>
2018-07-17 14:04:24 +0200

    add L4Bender linker in all relevant scenarios

--- src/librustc_codegen_ssa/back/linker.rs
+++ src/librustc_codegen_ssa/back/linker.rs
@@ -89,7 +89,7 @@ impl LinkerInfo {
                     cmd,
                     sess,
                     hinted_static: false,
-                }) as Box<Linker>
+                }) as Box<dyn Linker>
             },
             LinkerFlavor::Lld(LldFlavor::Wasm) => {
                 Box::new(WasmLd::new(cmd, sess, self)) as Box<dyn Linker>
@@ -1321,6 +1321,9 @@ impl<'a> Linker for L4Bender<'a> {
 
     fn group_start(&mut self) { self.cmd.arg("--start-group"); }
     fn group_end(&mut self) { self.cmd.arg("--end-group"); }
+    fn cross_lang_lto(&mut self) {
+        // do nothing
+    }
 }
 
 impl<'a> L4Bender<'a> {
